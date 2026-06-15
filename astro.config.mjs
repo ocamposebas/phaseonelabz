5990b7a2e268a85806f734509cda66d021a142bc
unknown <sebasocampo1414@gmail.com>
2026-06-15 11:17:18 -0500

    Add Astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,22 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import node from "@astrojs/node";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://phaseonelabz.com",
+
   output: "server",
 
-  integrations: [react()],
+  integrations: [
+    react(),
+    sitemap({
+      changefreq: "weekly",
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
+  ],
 
   vite: {
     plugins: [tailwindcss()],
